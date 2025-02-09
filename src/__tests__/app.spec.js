723f2417c22539608bc00fe9d2b4f0de0db52c64
massivebird <33991373+massivebird@users.noreply.github.com>
2025-02-08 23:13:18 -0500

    added: boilerplate playwright test

---
+++ src/__tests__/app.spec.js
@@ -0,0 +1,18 @@
+const { test, expect, _electron: electron } = require('@playwright/test')
+
+// This boilerplate lifted from https://circleci.com/blog/electron-testing/
+
+test('an h1 contains hello world"', async () => {
+  const electronApp = await electron.launch({ args: ['.'] })
+
+  // Wait for the first BrowserWindow to open
+  const window = await electronApp.firstWindow()
+
+  // Check for the presence of an h1 element with the text "hello"
+  const headerElement = await window.$('h1')
+  const headerText = await headerElement.textContent()
+  expect(headerText).toBe("KeepOrDelete")
+
+  // Close the app
+  await electronApp.close()
+})
