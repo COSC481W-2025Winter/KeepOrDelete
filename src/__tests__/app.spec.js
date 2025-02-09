9ec6e8df63af0b41334b15d63ae2326f7209adc9
massivebird <33991373+massivebird@users.noreply.github.com>
2025-02-08 23:14:53 -0500

    style: formatted some whitespace

--- src/__tests__/app.spec.js
+++ src/__tests__/app.spec.js
@@ -3,16 +3,16 @@ const { test, expect, _electron: electron } = require('@playwright/test')
 // This boilerplate lifted from https://circleci.com/blog/electron-testing/
 
 test('an h1 contains hello world"', async () => {
-  const electronApp = await electron.launch({ args: ['.'] })
+   const electronApp = await electron.launch({ args: ['.'] })
 
-  // Wait for the first BrowserWindow to open
-  const window = await electronApp.firstWindow()
+   // Wait for the first BrowserWindow to open
+   const window = await electronApp.firstWindow()
 
-  // Check for the presence of an h1 element with the text "hello"
-  const headerElement = await window.$('h1')
-  const headerText = await headerElement.textContent()
-  expect(headerText).toBe("KeepOrDelete")
+   // Check for the presence of an h1 element with the text "hello"
+   const headerElement = await window.$('h1')
+   const headerText = await headerElement.textContent()
+   expect(headerText).toBe("KeepOrDelete")
 
-  // Close the app
-  await electronApp.close()
+   // Close the app
+   await electronApp.close()
 })
