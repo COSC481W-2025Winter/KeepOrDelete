fb8da835c75902ae2b2dcf4718246249c67758f6
bo french <bfrench3@emich.edu>
2025-03-10 23:10:06 -0400

    update comment

--- test/delete.test.js
+++ test/delete.test.js
@@ -66,24 +66,24 @@ test("will delete common file types", async ({ page }) => {
     await window.waitForTimeout(300);
     await window.locator("#trash_button").click();
 
-    let ul = await window.locator("ul"); // Adjust the locator if necessary to target the right <ul>
+    let ul = await window.locator("ul");
     let ulText = await ul.innerHTML();
     expect(ulText).not.toContain("No deleted files.");
-    const undoButtonLocator = window.locator(".deleteUndo"); // Use class to target multiple undo buttons
+    const undoButtonLocator = window.locator(".deleteUndo");
     await undoButtonLocator.first().waitFor();
-    await undoButtonLocator.first().click(); // Click the first undo button
+    await undoButtonLocator.first().click();
     await window.waitForTimeout(300);
-    await undoButtonLocator.first().click(); // Click the first undo button
+    await undoButtonLocator.first().click();
     await window.waitForTimeout(300);
     const deletedFilesList = await window.locator("#deletedFilesList");
-    await deletedFilesList.waitFor(); // Ensure the deleted files list is available
+    await deletedFilesList.waitFor();
     ulText = await deletedFilesList.innerHTML();
     expect(ulText).toContain("No deleted files.");
     await window.locator("#navMainMenu").click();
     await window.waitForTimeout(300);
     await window.locator("#nextButton").click();
     await window.waitForTimeout(200);
-    await window.locator("#deleteButton").click(); //click delete
+    await window.locator("#deleteButton").click();
     await window.waitForTimeout(300);
     await window.locator("#finalPageButton").click();
     ul = await window.locator("#keptFilesList");
@@ -92,10 +92,6 @@ test("will delete common file types", async ({ page }) => {
     ul = await window.locator("#deletedFilesList");
     ulText = await ul.innerHTML();
     expect(ulText).not.toContain("No deleted files.")
-    //const fileExists = await fs.stat(testFiles[testFiles.indexOf(index)]).then(() => true).catch(() => false);
-    //expect(fileExists).toBe(false); //same thing as last test, but ensure it is FALSE now
-    //await window.waitForTimeout(100);
-
 });
 
 
