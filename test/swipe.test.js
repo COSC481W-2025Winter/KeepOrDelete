0c72ae43ac7cae2436d1899656f5831e638d4d69
David Williams <120070689+WilliamsDave@users.noreply.github.com.>
2025-03-18 12:33:50 -0400

    fix swipe test to be faster and more modular

--- test/swipe.test.js
+++ test/swipe.test.js
@@ -12,43 +12,57 @@ const testDirPath = path.join(os.tmpdir(), "keepordelete-preview-tests");
 const cleanTestDir = function() {
    // Clean temporary directory if it exists.
    if (fs.existsSync(testDirPath)) {
-      fs.rmSync(testDirPath, { recursive: true, force: true }, (err) => {
-         if (err) throw err;
-      })
+      fs.rmSync(testDirPath, { recursive: true, force: true });
    }
 }
 
-test.beforeEach(async () => {
+test.beforeAll(async () => {
    electronApp = await electron.launch({ args: ["./"] });
+});
+
+test.beforeEach(async () => {
    swapper = 0;
    cleanTestDir();
 
    // Create temporary directory.
-   fs.mkdirSync(testDirPath, { recursive: true }, (err) => {
-      if (err) throw err;
-   });
-
-   // Verify that temporary directory exists.
-   expect(fs.existsSync(testDirPath));
+   fs.mkdirSync(testDirPath, { recursive: true });
 
    // Create various files inside the temporary directory.
    for (let i = 0; i < 3; i++) {
       fs.writeFileSync(path.join(testDirPath, `file${i}`), "contents")
    }
-
-   console.log(fs.readdir)
 });
 
 //closing app
 test.afterEach(async () => {
-   await electronApp.close();
-
+   await new Promise(resolve => setTimeout(resolve, 500));
    cleanTestDir();
 });
 
+test.afterAll(async () => {
+   await electronApp.close();
+});
+
+// handle file traversale for each input type
+const testFileProcessing = async (window, swipeAction) => {
+   let previousPath = null;
+   for (let i = 0; i < 3; i++) {
+      const path = await window.locator("#currentItem").innerText();
+      expect(previousPath !== path).toBe(true);
+      previousPath = path;
+
+      if (swapper === 0) {
+         await swipeAction("left");
+         swapper++;
+      } else {
+         await swipeAction("right");
+      }
+      await window.waitForTimeout(300);
+   }
+};
+
 test("Button press to keep on KeepOrDelete page", async () => {
    const window = await electronApp.firstWindow();
-   await window.evaluate(() => localStorage.clear());
    await window.goto("file://" + path.resolve(__dirname, "../src/main_menu.html"));
 
    // Intercept file selection dialog
@@ -63,42 +77,15 @@ test("Button press to keep on KeepOrDelete page", async () => {
    await window.locator("#SelectButton").click();
    await window.locator("#goButton").click();
    await window.waitForURL("**/keep_or_delete.html");
-
-   let previousPath = null;
-   for (let i = 0; i < 3; i++) {
-      const path = await window.locator("#currentItem").innerText();
-
-      console.log(`path=${path}`);
-
-      let preview = await window.locator("#previewContainer").innerText();
-      // Remove emojis using regex
-      preview = preview.replace(/✅|🗑️/g, "").trim(); 
-      // Freak out if the file path didn't change.
-      if (previousPath != null && path == previousPath) {
-         expect(false).toBe(true);
-      }
-      previousPath = path;
-      if (swapper == 0) {
-         // Cycle to next file.
-         await window.locator("#deleteButton").click();
-         // Need timeout to account for animation!!
-         await window.waitForTimeout(500);
-         swapper++;
-      }
-      else{
-         // Cycle to next file.
-         await window.locator("#nextButton").click();
-         // Need timeout to account for animation!!
-         await window.waitForTimeout(500);
-      }
-   }
+  
+   await testFileProcessing(window, async (direction) => {
+      await window.locator(direction === "left" ? "#deleteButton" : "#nextButton").click();
+   });
    await window.evaluate(() => localStorage.clear());
 });
 
 test("Touch swipe to keep on KeepOrDelete page", async () => {
    const window = await electronApp.firstWindow();
-   await window.evaluate(() => localStorage.clear());
-
    await window.goto("file://" + path.resolve(__dirname, "../src/main_menu.html"));
 
    // Intercept file selection dialog
@@ -114,45 +101,14 @@ test("Touch swipe to keep on KeepOrDelete page", async () => {
    await window.locator("#goButton").click();
    await window.waitForURL("**/keep_or_delete.html");
 
-   let previousPath = null;
-
-   for (let i = 0; i < 3; i++) {
-      const path = await window.locator("#currentItem").innerText();
-
-      console.log(`path=${path}`);
-
-      let preview = await window.locator("#previewContainer").innerText();
-      // Remove emojis using regex
-      preview = preview.replace(/✅|🗑️/g, "").trim(); 
-
-      // Freak out if the file path didn't change.
-      if (previousPath != null && path == previousPath) {
-         expect(false).toBe(true);
-      }
-
-      previousPath = path;
-      if (swapper == 0) {
-         const previewContainer = await window.locator("#previewContainer");
-         const box = await previewContainer.boundingBox();
-         await previewContainer.hover();
-         await window.mouse.down();
-         await window.mouse.move(box.x + 500, box.y);
-         await window.mouse.up();
-         // Need timeout to account for animation!!
-         await window.waitForTimeout(500);
-         swapper++;
-      }
-      else{
-         const previewContainer = await window.locator("#previewContainer");
-         const box = await previewContainer.boundingBox();
-         await previewContainer.hover();
-         await window.mouse.down();
-         await window.mouse.move(box.x - 500, box.y);   
-         await window.mouse.up();
-         // Need timeout to account for animation!!
-         await window.waitForTimeout(500);
-      }
-   }
+   await testFileProcessing(window, async (direction) => {
+      const previewContainer = await window.locator("#previewContainer");
+      let box = await previewContainer.boundingBox();
+      await previewContainer.hover();
+      await window.mouse.down();
+      await window.mouse.move(box.x + (swapper === 0 ? box.width * 0.75 : -box.width * 0.75), box.y);
+      await window.mouse.up();
+   });
    await window.evaluate(() => localStorage.clear());
 });
 test("Arrow key Swipe to keep on KeepOrDelete page", async () => {
@@ -173,37 +129,9 @@ test("Arrow key Swipe to keep on KeepOrDelete page", async () => {
    await window.locator("#goButton").click();
    await window.waitForURL("**/keep_or_delete.html");
 
-   let previousPath = null;
+   await testFileProcessing(window, async (direction) => {
+      await window.keyboard.press(direction === "left" ? "ArrowLeft" : "ArrowRight");
+   });
 
-   for (let i = 0; i < 3; i++) {
-      const path = await window.locator("#currentItem").innerText();
-
-      console.log(`path=${path}`);
-
-      let preview = await window.locator("#previewContainer").innerText();
-      // Remove emojis using regex
-      preview = preview.replace(/✅|🗑️/g, "").trim(); 
-
-      // Freak out if the file path didn't change.
-      if (previousPath != null && path == previousPath) {
-         expect(false).toBe(true);
-      }
-
-      previousPath = path;
-
-      if (swapper == 0) {
-         // Cycle to next file.
-         await window.keyboard.press("ArrowLeft");
-         // Need timeout to account for animation!!
-         await window.waitForTimeout(500);
-         swapper++;
-      }
-      else{
-         // Cycle to next file.
-         await window.keyboard.press("ArrowRight");
-         // Need timeout to account for animation!!
-         await window.waitForTimeout(500);
-      }
-   }
    await window.evaluate(() => localStorage.clear());
 });
