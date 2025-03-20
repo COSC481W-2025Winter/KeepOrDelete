6ed11d0fae0f6d4213e279a84798948420bb2fa4
massivebird <33991373+massivebird@users.noreply.github.com>
2025-03-20 02:21:46 -0400

    changed: default window size

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ let mainWindow;
 
 const createWindow = () => {
    mainWindow = new BrowserWindow({
-      width: 800,
-      height: 800,
+      width: 900,
+      height: 900,
       webPreferences: {
          preload: path.join(__dirname, "preload.js"),
          sandbox: false,
