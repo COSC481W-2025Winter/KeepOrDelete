bce551d57a27736c32f2757c7b3b21fde9d00d24
massivebird <33991373+massivebird@users.noreply.github.com>
2025-04-04 17:22:10 -0400

    style: changed some comments

--- src/fileObjects.js
+++ src/fileObjects.js
@@ -1,4 +1,3 @@
-// File data object
 class FileObject {
    constructor({ name, path, modifiedDate, createdDate, size, status = null, ext }) {
       this.name = name;
@@ -11,28 +10,36 @@ class FileObject {
    }
 }
 
-let fileObjects = []; // Array for FileObject instances
+// Collection of `FileObject` instances.
+let fileObjects = [];
 
+/// Clears the `fileObjects` array.
 export function reset() {
    fileObjects = []
 }
 
+/// Returns a copy of a single `fileObject` element.
 export function get(i) {
    return fileObjects[i];
 }
 
+/// Returns a copy of the `fileObjects` array.
 export function getAll() {
    return fileObjects;
 }
 
+/// Sets the status of a single `fileObjects` element.
 export function setStatus(i, status) {
    fileObjects[i].status = status;
 }
 
+/// Returns `true` if `fileObjects` is empty. Returns `false` otherwise.
 export function isEmpty() {
    return fileObjects.length === 0;
 }
 
+/// Given an array of filepaths, resets `fileObjects` as an array of
+// `FileObject` instances created from the filepaths.
 export function setFromFiles(files) {
    fileObjects = files.map(f => new FileObject(f));
 }
