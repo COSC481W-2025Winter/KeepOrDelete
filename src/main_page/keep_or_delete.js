1ae1a874a1d48f162c5ad0a7d961137ef58523b1
massivebird <33991373+massivebird@users.noreply.github.com>
2025-04-04 20:48:37 -0400

    removed: redundant function call

--- src/main_page/keep_or_delete.js
+++ src/main_page/keep_or_delete.js
@@ -3,27 +3,25 @@ import * as swipe from "../swipe.js"
 import * as progress from "../progress.js"
 import * as ui from "../ui.js"
 
-window.onload = async function() {
-   // Cache DOM references
-   const dirPath = await window.file.getFilePath();
+// Cache DOM references
+const dirPath = await window.file.getFilePath();
 
-   if (!dirPath) {
-      // Hide all UI elements except welcomeScreen
-      ui.toggleUIElements(false);
+if (!dirPath) {
+   // Hide all UI elements except welcomeScreen
+   ui.toggleUIElements(false);
+} else {
+   // Show main UI and hide welcome screen
+   ui.toggleUIElements(true);
+   document.getElementById("dirPath").innerText = `Selected Directory: \n${dirPath}`;
+   if (!fileObject.isEmpty()) {
+      swipe.displayCurrentFile();
    } else {
-      // Show main UI and hide welcome screen
-      ui.toggleUIElements(true);
-      document.getElementById("dirPath").innerText = `Selected Directory: \n${dirPath}`;
-      if (!fileObject.isEmpty()) {
-         swipe.displayCurrentFile();
-      } else {
-         progress.updateProgress();
-         document.getElementById("currentItem").innerText = "No files found.";
-      }
+      progress.updateProgress();
+      document.getElementById("currentItem").innerText = "No files found.";
    }
+}
 
-   // Reveal body after all elements are ready only for keep_or_delete.html
-   if (document.body.classList.contains("keep-or-delete")) {
-      document.body.classList.add("show");
-   }
-};
+// Reveal body after all elements are ready only for keep_or_delete.html
+if (document.body.classList.contains("keep-or-delete")) {
+   document.body.classList.add("show");
+}
